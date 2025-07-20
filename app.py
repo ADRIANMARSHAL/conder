c1a4e04be3e80ff6f697bd11bdf7163dd34b35d5
Adrian Marshal <adrimarsh898@gmail.com>
2025-07-20 12:52:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -67,5 +63,5 @@ def results():
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
+        db.create_all() 
     app.run(debug=True)
