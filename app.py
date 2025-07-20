4eb2037215115bb9800156f666bdf265c7e81d64
Adrian Marshal <adrimarsh898@gmail.com>
2025-07-20 13:19:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 app.secret_key = 'agneswayua'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crushes.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
