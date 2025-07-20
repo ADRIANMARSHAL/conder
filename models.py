ca4f9bc99a9daf3768017000c19533a7d1158371
Adrian Marshal <adrimarsh898@gmail.com>
2025-07-20 12:44:37 +0300

    Update models.py

--- models.py
+++ models.py
@@ -4,5 +4,5 @@ db = SQLAlchemy()
 
 class Crush(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    instagram = db.Column(db.String(150), nullable=False)
-    submitted_by = db.Column(db.String(150), nullable=False)
+    submitted_by = db.Column(db.String(100), nullable=False)
+    instagram = db.Column(db.String(100), nullable=False)
